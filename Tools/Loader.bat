854f914c1e9d942d7e26bbea5c18370d6ebbb61d
JohnnyGo <JohnnyGo176@gmail.com>
2019-03-27 21:44:35 -0700

    Adding the .exe I build to create the pce.pkg file, and a new .bat file for the TurboGrafx16

--- Tools/Loader.bat
+++ Tools/Loader.bat
@@ -7,11 +7,12 @@ echo ::Welcome to Injectiine::
 echo :::::::::::::::::::::::::
 echo.
 echo Please select a console.
-echo NES  (1)
-echo SNES (2)
-echo N64  (3)
-echo GBA  (4)
-echo NDS  (5)
+echo NES          (1)
+echo SNES         (2)
+echo N64          (3)
+echo GBA          (4)
+echo NDS          (5)
+echo TurboGrafx16 (6)
 echo.
 set /p CHOICE=[Your Choice:] 
 if %CHOICE%==1 GOTO:NES
@@ -19,6 +20,7 @@ if %CHOICE%==2 GOTO:SNES
 if %CHOICE%==3 GOTO:N64
 if %CHOICE%==4 GOTO:GBA
 if %CHOICE%==5 GOTO:NDS
+if %CHOICE%==6 GOTO:TurboGrafx16
 GOTO:Menu
 
 :NES
@@ -49,4 +51,10 @@ exit
 cd CONSOLES
 cd NDS
 call NDS.bat
+exit
+
+:TurboGrafx16
+cd CONSOLES
+cd TurboGrafx16
+call TurboGrafx16.bat
 exit
